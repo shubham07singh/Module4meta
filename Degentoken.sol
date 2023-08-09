d34a5fec06b15222e59007f232581242cea88702
shubham07singh <126802667+shubham07singh@users.noreply.github.com>
2023-08-09 17:23:50 +0530

    Update Degentoken.sol

--- Degentoken.sol
+++ Degentoken.sol
@@ -1,92 +1,98 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
 interface IERC20 {
-    function totalSupply() external view returns (uint);
-    function balanceOf(address account) external view returns (uint);
-    function transfer(address recipient, uint amount) external returns (bool);
-    
-    event Transfer(address indexed from, address indexed to, uint amount);
+    function totalsupply() external view returns (uint256); 
+    function balanceof(address account) external view returns (uint256);
+    function mint(address to,uint256 amount) external;
+    function burn(uint256 amount) external;
+    function transfer (address recipient , uint256 amount) external returns (bool);
+    function addItem(string memory itemName, uint256 price , string memory desc) external;
+    function redeemItem(uint256 Item) external payable;
+    function getItemDetails(uint256 item) external view returns (string memory , uint256);
+    function owner() external view returns (address);
 }
 
-contract ERC20 is IERC20 {
-    address public immutable owner;
-    uint public totalSupply;
-    mapping (address => uint) public balanceOf;
-
-    struct Item {
+contract DegenToken is IERC20 {
+    string public name ;
+    string public symbol;
+    uint256 private _totalsupply;
+    mapping(address => uint256) private _balances;
+    mapping(uint256 => iteminfo) private _items;
+    address private _owner;
+    event itemredeemed(address indexed player , uint256 Item);
+    uint public itemCount =0;
+    struct iteminfo{
         uint itemId;
-        string itemName;
-        uint itemPrice;
-    }
-    
-    mapping(uint => Item) public items;
-    uint public itemCount;
+        string name;
+        uint price;
+        string desc;
 
+    }
     constructor() {
-        owner = msg.sender;
-        totalSupply = 0;
-    }
+       name = "Degen";
+       symbol = "DGN";
+       _totalsupply = 0;
+       _owner = msg.sender;
 
-    modifier onlyOwner {
-        require(msg.sender == owner, "Only the contract owner can execute this function");
+       addItem("DLQ" , 100 , "A Sniper");
+       addItem("Roll" , 200 , "A Shotgun");
+       addItem("KRM" , 100 , "A Smg");
+
+    }
+    modifier onlyOwner(){
+        require(msg.sender == _owner , "Only the owner can call the function");
         _;
     }
+    function totalsupply() external view returns (uint256){
+        return _totalsupply;
+    }
+    function balanceof(address account) external view returns (uint256){
+        return _balances[account];
 
-    string public constant name = "Degen";
-    string public constant symbol = "DGN";
-    uint8 public constant decimals = 0;
+    }
+    function mint(address to,uint256 amount) external onlyOwner {
+        require (amount > 0 , "Amount must be greater than zero");
+        _totalsupply +=amount;
+        _balances[to] +=amount;
 
-    function transfer(address recipient, uint amount) external returns (bool) {
-        require(balanceOf[msg.sender] >= amount, "The balance is insufficient");
-
-        balanceOf[msg.sender] -= amount;
-        balanceOf[recipient] += amount;
-
-        emit Transfer(msg.sender, recipient, amount);
+    }
+    function burn (uint256 amount) external {
+        require(amount > 0 , "Amount must be greater than zero");
+        require(_balances[msg.sender] >= amount , "The balance is insufficient");
+       _totalsupply -=amount;
+       _balances[msg.sender]-=amount;
+        
+    }
+    function transfer (address recipient , uint256 amount) external returns (bool){
+         require(amount > 0 , "Amount must be greater than zero");
+        require(_balances[msg.sender] >= amount , "The balance is insufficient");
+        _balances[msg.sender] -= amount ;
+        _balances[recipient] += amount;
         return true;
     }
+      function addItem(string memory itemName, uint256 price , string memory desc) public{
+          itemCount++;
+          _items[itemCount]= iteminfo(itemCount,itemName , price ,desc);
+      }
+      function redeemItem(uint256 Item) external payable{
+          require(bytes(_items[Item].name).length > 0 , "Item does not exist");
+          require(_balances[msg.sender] >= _items[Item].price , "Insufficient balance");
+          _balances[msg.sender] -= _items[Item].price;
+          emit itemredeemed(msg.sender , Item);
+      }
+       function getItemDetails(uint256 item) external view returns (string memory , uint256){
+           return (_items[item].name , _items[item].price);
+       }
+       function getItems() external view returns (iteminfo[] memory){
+          iteminfo[] memory allitems = new iteminfo[](itemCount);
 
-    function mint(address receiver,uint amount) external onlyOwner {
-        balanceOf[receiver] += amount;
-        totalSupply += amount;
-        emit Transfer(address(0), receiver, amount);
-    }
-
-    function burn(uint amount) external {
-        require(amount > 0, "Amount should not be zero");
-        require(balanceOf[msg.sender] >= amount, "The balance is insufficient");
-        balanceOf[msg.sender] -= amount;
-        totalSupply -= amount;
-
-        emit Transfer(msg.sender, address(0), amount);
-    }
-    
-    function addItem(string memory itemName, uint256 itemPrice) external onlyOwner {
-        itemCount++;
-        Item memory newItem = Item(itemCount, itemName, itemPrice);
-        items[itemCount] = newItem;
-    }
-
-    function getItems() external view returns (Item[] memory) {
-        Item[] memory allItems = new Item[](itemCount);
-        
-        for (uint i = 1; i <= itemCount; i++) {
-            allItems[i - 1] = items[i];
-        }
-        
-        return allItems;
-    }
-    
-    function redeem(uint itemId) external {
-        require(itemId > 0 && itemId <= itemCount, "Invalid item ID");
-        Item memory redeemedItem = items[itemId];
-        
-        require(balanceOf[msg.sender] >= redeemedItem.itemPrice, "Insufficient balance to redeem");
-        
-        balanceOf[msg.sender] -= redeemedItem.itemPrice;
-        balanceOf[owner] += redeemedItem.itemPrice;
-        emit Transfer(msg.sender, address(0), redeemedItem.itemPrice);
-        
-    }
+          for(uint i=1; i<= itemCount; i++){
+              allitems[i-1]= _items[i];
+          }
+          return allitems;
+       }
+     function owner() external view returns (address){
+         return _owner;
+     }
 }
